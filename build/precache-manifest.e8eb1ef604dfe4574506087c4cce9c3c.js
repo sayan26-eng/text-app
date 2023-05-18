c602119101616990e59334527036f46aac724953
sayan26-eng <www.sansfirefox@gmail.com>
2023-05-18 11:51:44 +0530

    first commit

---
+++ build/precache-manifest.e8eb1ef604dfe4574506087c4cce9c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccf09fe1e58c72549cdf",
+    "url": "/textUtils/static/css/main.807b8987.chunk.css"
+  },
+  {
+    "revision": "ccf09fe1e58c72549cdf",
+    "url": "/textUtils/static/js/main.ccf09fe1.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/textUtils/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "40d458445a5eaa38fc78",
+    "url": "/textUtils/static/js/2.40d45844.chunk.js"
+  },
+  {
+    "revision": "a671d1beae9d77bfc269",
+    "url": "/textUtils/static/js/runtime~main.a671d1be.js"
+  },
+  {
+    "revision": "a94156330436320cb38bf486d767cab9",
+    "url": "/textUtils/index.html"
+  }
+];
